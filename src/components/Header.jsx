edc9ae4c63affc806806cc32c8049f1d283be19c
Andrea Huang <huangandrea@hotmail.it>
2025-05-03 16:26:31 +0200

    Creating a Configurable & Flexible Custom Button Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button.jsx";
 
 export default function Header() {
   return (
-    <header id="main-header">
-      <div id="title">
-        <img src={logoImg} alt="Food and drinks" />
+    <header id='main-header'>
+      <div id='title'>
+        <img src={logoImg} alt='Food and drinks' />
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
